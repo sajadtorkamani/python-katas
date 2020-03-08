30acfccb1bc45f79958fd5e0bf9c4cee092731ec
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-03-08 19:17:58 +0000

    Solve kata

---
+++ nothing_special/readme.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/57029e77005264a3b9000eb5/train/python
---
+++ nothing_special/solution.py
@@ -0,0 +1,5 @@
+import re
+
+
+def nothing_special(s):
+    return re.sub('[^A-Za-z0-9\s]', '', s) if isinstance(s, str) else 'Not a string!'
