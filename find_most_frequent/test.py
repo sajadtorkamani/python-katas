f2e6f4f66e6cb95b06bb0917f6b1061e122283cc
Sajad Torkamani <sajadtorkamani1@gmail.com>
2020-03-08 13:13:24 +0000

    Refactor: tidy up code

--- find_most_frequent/test.py
+++ find_most_frequent/test.py
@@ -4,6 +4,6 @@ from .solution import find_most_frequent
 
 class SolutionTest(unittest.TestCase):
     def test_returns_correct_result(self):
-        self.assertEquals(find_most_frequent([1, 1, 2, 3]), {1})
-        self.assertEquals(find_most_frequent([1, 1, 2, 2, 3]), {1, 2})
-        self.assertEquals(find_most_frequent([]), set([]))
+        self.assertEqual(find_most_frequent([1, 1, 2, 3]), {1})
+        self.assertEqual(find_most_frequent([1, 1, 2, 2, 3]), {1, 2})
+        self.assertEqual(find_most_frequent([]), set([]))
